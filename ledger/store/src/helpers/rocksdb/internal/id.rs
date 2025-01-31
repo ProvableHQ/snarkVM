3825922ad55ea04fe2609cf4afd08d1276103b31
Pranav Gaddamadugu <23022326+d0cd@users.noreply.github.com>
2025-01-31 14:16:01 -0800

    Add to deployment DB

--- ledger/store/src/helpers/rocksdb/internal/id.rs
+++ ledger/store/src/helpers/rocksdb/internal/id.rs
@@ -106,6 +106,7 @@ pub enum CommitteeMap {
 #[repr(u16)]
 pub enum DeploymentMap {
     ID = DataID::DeploymentIDMap as u16,
+    IDV2 = DataID::DeploymentIDV2Map as u16,
     Edition = DataID::DeploymentEditionMap as u16,
     ReverseID = DataID::DeploymentReverseIDMap as u16,
     Owner = DataID::DeploymentOwnerMap as u16,
@@ -305,4 +306,7 @@ enum DataID {
     Test4,
     #[cfg(test)]
     Test5,
+
+    // Additional
+    DeploymentIDV2Map,
 }
