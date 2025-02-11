432b4a3f9e0065b45b4865a16314772bef220f26
Pranav Gaddamadugu <23022326+d0cd@users.noreply.github.com>
2025-02-10 19:41:12 -0800

    Add some VM tests

--- synthesizer/src/vm/tests/test_vm_update.rs
+++ synthesizer/src/vm/tests/test_vm_update.rs
@@ -16,6 +16,10 @@
 use super::*;
 use synthesizer_program::StackProgram;
 
+// This test checks that:
+//  - the logic of a simple transition without records can be updated.
+//  - once a program is updated, the old transitions are no longer valid.
+//  - an invalid authority cannot update a program.
 #[test]
 fn test_simple_update() -> Result<()> {
     let rng = &mut TestRng::default();
@@ -95,6 +99,22 @@ function binary_add:
     ",
     )?;
 
+    // Attempt to deploy the updated program with an invalid authority.
+    let invalid_private_key = PrivateKey::new(rng)?;
+    let transaction = vm.deploy_updatable(
+        &invalid_private_key,
+        Address::try_from(&caller_private_key)?,
+        1,
+        &updated_program,
+        None,
+        0,
+        None,
+        rng,
+    )?;
+    let block = sample_next_block(&vm, &invalid_private_key, &[transaction], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 1);
+    vm.add_next_block(&block)?;
+
     // Deploy the updated program.
     let transaction = vm.deploy_updatable(
         &caller_private_key,
@@ -137,6 +157,507 @@ function binary_add:
         output => bail!(format!("Unexpected output: {output}")),
     };
     assert_eq!(output, 3u8);
+
+    Ok(())
+}
+
+// This test checks that a program deployed with `VM::deploy`, which uses `ProgramOwner::V1` cannot be updated.
+#[test]
+fn test_program_owner_v1_is_not_updatable() -> Result<()> {
+    let rng = &mut TestRng::default();
+
+    // Initialize a new caller.
+    let caller_private_key = sample_genesis_private_key(rng);
+    let caller_address = Address::try_from(&caller_private_key)?;
+
+    // Initialize the genesis block.
+    let genesis = sample_genesis_block(rng);
+
+    // Initialize the VM.
+    let vm = sample_vm();
+    vm.add_next_block(&genesis)?;
+
+    // Initialize the program.
+    let program = Program::from_str("program basic.aleo;function foo:")?;
+
+    // Deploy the program.
+    let transaction = vm.deploy(&caller_private_key, &program, None, 0, None, rng)?;
+    let block = sample_next_block(&vm, &caller_private_key, &[transaction], rng)?;
+    vm.add_next_block(&block)?;
+
+    // Initialize the updated program.
+    let updated_program = Program::from_str("program basic.aleo;function foo:function bar:")?;
+
+    // Attempt to deploy the updated program using `VM::deploy`.
+    let transaction = vm.deploy(&caller_private_key, &updated_program, None, 0, None, rng)?;
+    let block = sample_next_block(&vm, &caller_private_key, &[transaction], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 1);
+    vm.add_next_block(&block)?;
+
+    // Attempt to deploy the updated program using `VM::deploy_updatable`.
+    let transaction =
+        vm.deploy_updatable(&caller_private_key, caller_address, 1, &updated_program, None, 0, None, rng)?;
+    let block = sample_next_block(&vm, &caller_private_key, &[transaction], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 1);
+    vm.add_next_block(&block)?;
+
+    Ok(())
+}
+
+// This test checks that:
+//  - the first instance of a program must be the zero-th edition.
+//  - subsequent updates to the program must be sequential.
+#[test]
+fn test_editions_are_sequential() -> Result<()> {
+    let rng = &mut TestRng::default();
+
+    // Initialize a new caller.
+    let caller_private_key = sample_genesis_private_key(rng);
+    let caller_address = Address::try_from(&caller_private_key)?;
+
+    // Initialize the genesis block.
+    let genesis = sample_genesis_block(rng);
+
+    // Initialize two VMs.
+    let off_chain_vm = sample_vm();
+    let on_chain_vm = sample_vm();
+    off_chain_vm.add_next_block(&genesis)?;
+    on_chain_vm.add_next_block(&genesis)?;
+
+    // Define the three versions of the program.
+    let program_v0 = Program::from_str("program basic.aleo;function foo:")?;
+    let program_v1 = Program::from_str("program basic.aleo;function foo:function bar:")?;
+    let program_v2 = Program::from_str("program basic.aleo;function foo:function bar:function baz:")?;
+
+    // Using the off-chain VM, generate a sequence of deployments.
+    let deployment_v0_pass =
+        off_chain_vm.deploy_updatable(&caller_private_key, caller_address, 0, &program_v0, None, 0, None, rng)?;
+    off_chain_vm.process().write().add_program(&program_v0)?;
+    let deployment_v1_fail =
+        off_chain_vm.deploy_updatable(&caller_private_key, caller_address, 1, &program_v1, None, 0, None, rng)?;
+    let deployment_v1_pass =
+        off_chain_vm.deploy_updatable(&caller_private_key, caller_address, 1, &program_v1, None, 0, None, rng)?;
+    let deployment_v2_as_v1_fail =
+        off_chain_vm.deploy_updatable(&caller_private_key, caller_address, 1, &program_v2, None, 0, None, rng)?;
+    off_chain_vm.process().write().add_program(&program_v1)?;
+    let deployment_v2_fail =
+        off_chain_vm.deploy_updatable(&caller_private_key, caller_address, 2, &program_v2, None, 0, None, rng)?;
+    let deployment_v2_pass =
+        off_chain_vm.deploy_updatable(&caller_private_key, caller_address, 2, &program_v2, None, 0, None, rng)?;
+
+    // Deploy the programs to the on-chain VM individually in the following sequence:
+    // - deployment_v1_fail
+    // - deployment_v0_pass
+    // - deployment_v2_fail
+    // - deployment_v1_pass
+    // - deployment_v2_as_v1_fail
+    // - deployment_v2_pass
+    // Their name should indicate whether the deployment should pass or fail.
+
+    // This deployment should fail because the it is not the zero-th edition.
+    let block = sample_next_block(&on_chain_vm, &caller_private_key, &[deployment_v1_fail], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 1);
+    on_chain_vm.add_next_block(&block)?;
+
+    // This deployment should pass.
+    let block = sample_next_block(&on_chain_vm, &caller_private_key, &[deployment_v0_pass], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 0);
+    on_chain_vm.add_next_block(&block)?;
+    let stack = on_chain_vm.process().read().get_stack("basic.aleo")?;
+    assert_eq!(stack.edition(), 0);
+
+    // This deployment should fail because it does not increment the edition.
+    let block = sample_next_block(&on_chain_vm, &caller_private_key, &[deployment_v2_fail], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 1);
+    on_chain_vm.add_next_block(&block)?;
+
+    // This deployment should pass.
+    let block = sample_next_block(&on_chain_vm, &caller_private_key, &[deployment_v1_pass], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 0);
+    on_chain_vm.add_next_block(&block)?;
+    let stack = on_chain_vm.process().read().get_stack("basic.aleo")?;
+    assert_eq!(stack.edition(), 1);
+
+    // This deployment should fail because it attempt to redeploy at the same edition.
+    let block = sample_next_block(&on_chain_vm, &caller_private_key, &[deployment_v2_as_v1_fail], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 1);
+    on_chain_vm.add_next_block(&block)?;
+
+    // This deployment should pass.
+    let block = sample_next_block(&on_chain_vm, &caller_private_key, &[deployment_v2_pass], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 0);
+    on_chain_vm.add_next_block(&block)?;
+    let stack = on_chain_vm.process().read().get_stack("basic.aleo")?;
+    assert_eq!(stack.edition(), 2);
+
+    Ok(())
+}
+
+// This test checks that:
+//  - records created before an update are still valid after an update.
+//  - records created after an update can be created and used in the updated program.
+//  - records are semantically distinct (old records cannot be used in functions that require new records).
+//  - functions can be disabled using `assert.neq self.caller self.caller`.
+#[test]
+fn test_update_with_records() -> Result<()> {
+    let rng = &mut TestRng::default();
+
+    // Initialize a new caller.
+    let caller_private_key = sample_genesis_private_key(rng);
+    let caller_view_key = ViewKey::try_from(&caller_private_key)?;
+    let caller_address = Address::try_from(&caller_private_key)?;
+
+    // Initialize the genesis block.
+    let genesis = sample_genesis_block(rng);
+
+    // Initialize the VM.
+    let vm = sample_vm();
+    vm.add_next_block(&genesis)?;
+
+    // Define the two versions of the program.
+    let program_v0 = Program::from_str(
+        r"
+program record_test.aleo;
+
+record data_v1:
+    owner as address.private;
+    data as u8.public;
+
+function mint:
+    input r0 as u8.public;
+    cast self.caller r0 into r1 as data_v1.record;
+    output r1 as data_v1.record;",
+    )?;
+
+    let program_v1 = Program::from_str(
+        r"
+program record_test.aleo;
+
+record data_v1:
+    owner as address.private;
+    data as u8.public;
+
+record data_v2:
+    owner as address.private;
+    data as u8.public;
+
+function mint:
+    input r0 as u8.public;
+    assert.neq self.caller self.caller;
+    cast self.caller r0 into r1 as data_v1.record;
+    output r1 as data_v1.record;
+
+function convert:
+    input r0 as data_v1.record;
+    cast r0.owner r0.data into r1 as data_v2.record;
+    output r1 as data_v2.record;
+
+function burn:
+    input r0 as data_v2.record;",
+    )?;
+
+    // Deploy the first version of the program.
+    let transaction = vm.deploy_updatable(&caller_private_key, caller_address, 0, &program_v0, None, 0, None, rng)?;
+    let block = sample_next_block(&vm, &caller_private_key, &[transaction], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 0);
+    vm.add_next_block(&block)?;
+
+    // Execute the mint function twice.
+    let mint_execution_0 = vm.execute(
+        &caller_private_key,
+        ("record_test.aleo", "mint"),
+        vec![Value::from_str("0u8")?].into_iter(),
+        None,
+        0,
+        None,
+        rng,
+    )?;
+    let mint_execution_1 = vm.execute(
+        &caller_private_key,
+        ("record_test.aleo", "mint"),
+        vec![Value::from_str("1u8")?].into_iter(),
+        None,
+        0,
+        None,
+        rng,
+    )?;
+    let block = sample_next_block(&vm, &caller_private_key, &[mint_execution_0, mint_execution_1], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 0);
+    let mut v1_records = block
+        .records()
+        .into_iter()
+        .map(|(_, record)| record.decrypt(&caller_view_key))
+        .collect::<Result<Vec<Record<CurrentNetwork, Plaintext<CurrentNetwork>>>>>()?;
+    assert_eq!(v1_records.len(), 2);
+    vm.add_next_block(&block)?;
+
+    // Update the program.
+    let transaction = vm.deploy_updatable(&caller_private_key, caller_address, 1, &program_v1, None, 0, None, rng)?;
+    let block = sample_next_block(&vm, &caller_private_key, &[transaction], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 0);
+    vm.add_next_block(&block)?;
+
+    // Attempt to execute the mint function.
+    assert!(
+        vm.execute(
+            &caller_private_key,
+            ("record_test.aleo", "mint"),
+            vec![Value::from_str("0u8")?].into_iter(),
+            None,
+            0,
+            None,
+            rng
+        )
+        .is_err()
+    );
+
+    // Get the first record and execute the convert function.
+    let record = v1_records.pop().unwrap();
+    let convert_execution = vm.execute(
+        &caller_private_key,
+        ("record_test.aleo", "convert"),
+        vec![Value::Record(record)].into_iter(),
+        None,
+        0,
+        None,
+        rng,
+    )?;
+    let block = sample_next_block(&vm, &caller_private_key, &[convert_execution], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 0);
+    let mut v2_records = block
+        .records()
+        .into_iter()
+        .map(|(_, record)| record.decrypt(&caller_view_key))
+        .collect::<Result<Vec<Record<CurrentNetwork, Plaintext<CurrentNetwork>>>>>()?;
+    assert_eq!(v2_records.len(), 1);
+    vm.add_next_block(&block)?;
+
+    // Get the v2 record and execute the burn function.
+    let record = v2_records.pop().unwrap();
+    let burn_execution = vm.execute(
+        &caller_private_key,
+        ("record_test.aleo", "burn"),
+        vec![Value::Record(record)].into_iter(),
+        None,
+        0,
+        None,
+        rng,
+    )?;
+    let block = sample_next_block(&vm, &caller_private_key, &[burn_execution], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 0);
+    vm.add_next_block(&block)?;
+
+    // Attempt to execute the burn function with the remaining v1 record.
+    let record = v1_records.pop().unwrap();
+    assert!(
+        vm.execute(
+            &caller_private_key,
+            ("record_test.aleo", "burn"),
+            vec![Value::Record(record)].into_iter(),
+            None,
+            0,
+            None,
+            rng
+        )
+        .is_err()
+    );
+
+    Ok(())
+}
+
+// This test checks that:
+//  - mappings created before an update are still valid after an update.
+//  - mappings created by and updated are correctly initialized and usable in the program.
+//  - functions can be disabled by inserting a failing condition in the on-chain logic.
+#[test]
+fn test_update_with_mappings() -> Result<()> {
+    let rng = &mut TestRng::default();
+
+    // Initialize a new caller.
+    let caller_private_key = sample_genesis_private_key(rng);
+    let caller_address = Address::try_from(&caller_private_key)?;
+
+    // Initialize the genesis block.
+    let genesis = sample_genesis_block(rng);
+
+    // Initialize the VM.
+    let vm = sample_vm();
+    vm.add_next_block(&genesis)?;
+
+    // Define the two versions of the program.
+    let program_v0 = Program::from_str(
+        r"
+program mapping_test.aleo;
+
+mapping data_v1:
+    key as u8.public;
+    value as u8.public;
+
+function store_data_v1:
+    input r0 as u8.public;
+    input r1 as u8.public;
+    async store_data_v1 r0 r1 into r2;
+    output r2 as mapping_test.aleo/store_data_v1.future;
+finalize store_data_v1:
+    input r0 as u8.public;
+    input r1 as u8.public;
+    set r1 into data_v1[r0];",
+    )?;
+
+    let program_v1 = Program::from_str(
+        r"
+program mapping_test.aleo;
+
+mapping data_v1:
+    key as u8.public;
+    value as u8.public;
+
+mapping data_v2:
+    key as u8.public;
+    value as u8.public;
+
+function store_data_v1:
+    input r0 as u8.public;
+    input r1 as u8.public;
+    async store_data_v1 r0 r1 into r2;
+    output r2 as mapping_test.aleo/store_data_v1.future;
+finalize store_data_v1:
+    input r0 as u8.public;
+    input r1 as u8.public;
+    assert.neq true true;
+
+function migrate_data_v1_to_v2:
+    input r0 as u8.public;
+    async migrate_data_v1_to_v2 r0 into r1;
+    output r1 as mapping_test.aleo/migrate_data_v1_to_v2.future;
+finalize migrate_data_v1_to_v2:
+    input r0 as u8.public;
+    get data_v1[r0] into r1;
+    remove data_v1[r0];
+    set r1 into data_v2[r0];
+
+function store_data_v2:
+    input r0 as u8.public;
+    input r1 as u8.public;
+    async store_data_v2 r0 r1 into r2;
+    output r2 as mapping_test.aleo/store_data_v2.future;
+finalize store_data_v2:
+    input r0 as u8.public;
+    input r1 as u8.public;
+    set r1 into data_v2[r0];",
+    )?;
+
+    // Deploy the first version of the program.
+    let transaction = vm.deploy_updatable(&caller_private_key, caller_address, 0, &program_v0, None, 0, None, rng)?;
+    let block = sample_next_block(&vm, &caller_private_key, &[transaction], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 0);
+    vm.add_next_block(&block)?;
+
+    // Execute the store_data_v1 function.
+    let store_data_v1_execution = vm.execute(
+        &caller_private_key,
+        ("mapping_test.aleo", "store_data_v1"),
+        vec![Value::from_str("0u8")?, Value::from_str("0u8")?].into_iter(),
+        None,
+        0,
+        None,
+        rng,
+    )?;
+    let block = sample_next_block(&vm, &caller_private_key, &[store_data_v1_execution], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 0);
+    vm.add_next_block(&block)?;
+
+    // Check that the value was stored correctly.
+    let value = match vm.finalize_store().get_value_confirmed(
+        ProgramID::from_str("mapping_test.aleo")?,
+        Identifier::from_str("data_v1")?,
+        &Plaintext::from_str("0u8")?,
+    )? {
+        Some(Value::Plaintext(Plaintext::Literal(Literal::U8(value), _))) => *value,
+        value => bail!(format!("Unexpected value: {:?}", value)),
+    };
+    assert_eq!(value, 0u8);
+
+    // Update the program.
+    let transaction = vm.deploy_updatable(&caller_private_key, caller_address, 1, &program_v1, None, 0, None, rng)?;
+    let block = sample_next_block(&vm, &caller_private_key, &[transaction], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 0);
+    vm.add_next_block(&block)?;
+
+    // Attempt to execute the store_data_v1 function.
+    let transaction = vm.execute(
+        &caller_private_key,
+        ("mapping_test.aleo", "store_data_v1"),
+        vec![Value::from_str("1u8")?, Value::from_str("1u8")?].into_iter(),
+        None,
+        0,
+        None,
+        rng,
+    )?;
+    let block = sample_next_block(&vm, &caller_private_key, &[transaction], rng)?;
+    assert_eq!(block.transactions().num_rejected(), 1);
+    vm.add_next_block(&block)?;
+
+    // Execute the migrate_data_v1_to_v2 function.
+    let migrate_data_v1_to_v2_execution = vm.execute(
+        &caller_private_key,
+        ("mapping_test.aleo", "migrate_data_v1_to_v2"),
+        vec![Value::from_str("0u8")?].into_iter(),
+        None,
+        0,
+        None,
+        rng,
+    )?;
+    let block = sample_next_block(&vm, &caller_private_key, &[migrate_data_v1_to_v2_execution], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 0);
+    vm.add_next_block(&block)?;
+
+    // Check that the value was migrated correctly.
+    let value = match vm.finalize_store().get_value_confirmed(
+        ProgramID::from_str("mapping_test.aleo")?,
+        Identifier::from_str("data_v2")?,
+        &Plaintext::from_str("0u8")?,
+    )? {
+        Some(Value::Plaintext(Plaintext::Literal(Literal::U8(value), _))) => *value,
+        value => bail!(format!("Unexpected value: {:?}", value)),
+    };
+    assert_eq!(value, 0u8);
+
+    // Check that the old value was removed.
+    assert!(
+        vm.finalize_store()
+            .get_value_confirmed(
+                ProgramID::from_str("mapping_test.aleo")?,
+                Identifier::from_str("data_v1")?,
+                &Plaintext::from_str("0u8")?
+            )?
+            .is_none()
+    );
+
+    // Execute the store_data_v2 function.
+    let store_data_v2_execution = vm.execute(
+        &caller_private_key,
+        ("mapping_test.aleo", "store_data_v2"),
+        vec![Value::from_str("1u8")?, Value::from_str("1u8")?].into_iter(),
+        None,
+        0,
+        None,
+        rng,
+    )?;
+    let block = sample_next_block(&vm, &caller_private_key, &[store_data_v2_execution], rng)?;
+    assert_eq!(block.aborted_transaction_ids().len(), 0);
+    vm.add_next_block(&block)?;
+
+    // Check that the value was stored correctly.
+    let value = match vm.finalize_store().get_value_confirmed(
+        ProgramID::from_str("mapping_test.aleo")?,
+        Identifier::from_str("data_v2")?,
+        &Plaintext::from_str("1u8")?,
+    )? {
+        Some(Value::Plaintext(Plaintext::Literal(Literal::U8(value), _))) => *value,
+        value => bail!(format!("Unexpected value: {:?}", value)),
+    };
+    assert_eq!(value, 1u8);
 
     Ok(())
 }
