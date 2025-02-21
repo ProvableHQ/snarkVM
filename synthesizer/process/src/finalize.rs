f1f3909e7728deb64a1afd2075ed64c177cafa93
Pranav Gaddamadugu <23022326+d0cd@users.noreply.github.com>
2025-02-21 06:30:57 -0800

    Fix finalization for V1 programs

--- synthesizer/process/src/finalize.rs
+++ synthesizer/process/src/finalize.rs
@@ -15,7 +15,7 @@
 
 use super::*;
 use console::program::{FinalizeType, Future, Register};
-use synthesizer_program::{Await, Constructor, FinalizeRegistersState, Operand};
+use synthesizer_program::{Await, Constructor, FinalizeRegistersState, Operand, ProgramVersion};
 use utilities::try_vm_runtime;
 
 use std::collections::HashSet;
@@ -88,10 +88,12 @@ impl<N: Network> Process<N> {
             }
             finish!(timer, "Initialize the program mappings");
 
-            // Execute the constructor and extend the finalize operations.
+            // If the program is a V2 program, execute the constructor and extend the finalize operations.
             // This must happen after the mappings are initialized as the constructor may depend on them.
-            let operations = finalize_constructor(state, store, &stack, *fee.transition_id())?;
-            finalize_operations.extend(operations);
+            if deployment.program().version() == ProgramVersion::V2 {
+                let operations = finalize_constructor(state, store, &stack, *fee.transition_id())?;
+                finalize_operations.extend(operations);
+            }
 
             // Return the stack and finalize operations.
             Ok((stack, finalize_operations))
@@ -230,6 +232,7 @@ fn finalize_constructor<N: Network, P: FinalizeStorage<N>>(
     let nonce = 0;
 
     // Get the constructor types.
+    // Note that this function is only called on V2 programs which are guaranteed to have constructor types.
     let constructor_types = stack.get_constructor_types()?.clone();
 
     // Initialize the finalize registers.
