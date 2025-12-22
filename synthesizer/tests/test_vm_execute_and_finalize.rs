31ab1e0fd7cbd34d7acf17ce28be7243ade5b24e
meddle <ntsvetinov@forte.io>
2025-12-22 12:16:55 +0200

    Try to make the integration test even faster

--- synthesizer/tests/test_vm_execute_and_finalize.rs
+++ synthesizer/tests/test_vm_execute_and_finalize.rs
@@ -50,19 +50,36 @@ type LedgerType = snarkvm_ledger_store::helpers::rocksdb::ConsensusDB<CurrentNet
 
 #[test]
 fn test_vm_execute_and_finalize_group1() {
-    run_group(0, 4);
+    run_group(0, 8);
 }
 #[test]
 fn test_vm_execute_and_finalize_group2() {
-    run_group(1, 4);
+    run_group(1, 8);
 }
 #[test]
 fn test_vm_execute_and_finalize_group3() {
-    run_group(2, 4);
+    run_group(2, 8);
 }
 #[test]
 fn test_vm_execute_and_finalize_group4() {
-    run_group(3, 4);
+    run_group(3, 8);
+}
+
+#[test]
+fn test_vm_execute_and_finalize_group5() {
+    run_group(4, 8);
+}
+#[test]
+fn test_vm_execute_and_finalize_group6() {
+    run_group(5, 8);
+}
+#[test]
+fn test_vm_execute_and_finalize_group7() {
+    run_group(6, 8);
+}
+#[test]
+fn test_vm_execute_and_finalize_group8() {
+    run_group(7, 8);
 }
 
 fn run_group(idx: usize, total: usize) {
